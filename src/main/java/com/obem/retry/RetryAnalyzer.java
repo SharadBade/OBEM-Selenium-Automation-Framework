0c6a1e2debfd1c14154d57371b3ba544cb654b24
SharadBade <sharadbade09@gmail.com>
2026-08-22 18:25:38 +0530

    Testing Jenkins

--- src/main/java/com/obem/retry/RetryAnalyzer.java
+++ src/main/java/com/obem/retry/RetryAnalyzer.java
@@ -6,7 +6,7 @@ import org.testng.ITestResult;
 import com.obem.utils.ConfigReader;
 
 public class RetryAnalyzer implements IRetryAnalyzer {
-
+// Testing Jenkins
     private int retryCount = 0;
     private final int maxRetryCount = ConfigReader.getInt("retryCount");
 
